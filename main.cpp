a54eea565dbd4723dc7f5c8a6dbf9e8646dedd9f
unknown <linakoskar@gmail.com>
2023-09-03 10:16:26 +0200

    Basic setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <Windows.h>
 
-int main(int argc, char** argv){
+//CONSIDER WHAT IS GOING ON UP THE CALLSTACK
+
+//an entry point windows graphical application
+//called by C runtime library(CRT)
+int CALLBACK 
+WinMain(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nShowCmd)
+  {
+
+    MessageBoxA(0,"Hello world!","Title",MB_OK|MB_ICONINFORMATION);
+
     return 0;
-}
+  }
