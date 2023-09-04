b7539c953af25dad539ef60e7e2d45243de5e639
unknown <linakoskar@gmail.com>
2023-09-04 18:05:57 +0200

    Another approach

--- main.cpp
+++ main.cpp
@@ -1,120 +1,46 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <tchar.h>
 #include <Windows.h>
 
-//Apparently all the strings should be ASCII code based
-//g++ .\main.cpp -o main.exe -lgdi32
-LRESULT Wndproc(
-  HWND hWindow,
-  UINT uMessage,
-  WPARAM wParam,
-  LPARAM lParam
-)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow){
 
-  LRESULT Result = 0;
+  //There is a mismatch between LPCSTR type and const wchar_t... for some reason?
+  //RESOLUTION: UNICODE needs to be defined in macro-way
+  const wchar_t CLASS_NAME[] = L"Window Class";
+  WNDCLASS wc = {};
 
-  //No debug messages
-  //Issue is here
-  switch(uMessage){
-    case WM_SIZE:
-    {
-      OutputDebugStringA("WM_SIZE");
-    }break;
-    case WM_DESTROY:
-    {
-      OutputDebugStringA("WM_DESTROY");
-    }break;
-    case WM_CLOSE:
-    {
-      OutputDebugStringA("WM_CLOSE");
-    }break;
-    case WM_ACTIVATEAPP:
-    {
-      OutputDebugStringA("WM_ACTIVATEAPP");
-    }break;
-    case WM_PAINT:
-    {
-      PAINTSTRUCT Paint;
-      HDC DeviceContext = BeginPaint(hWindow,&Paint);
-      LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-      LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
-      PatBlt(DeviceContext,Paint.rcPaint.left, Paint.rcPaint.right,Width,Height,BLACKNESS);
-      EndPaint(hWindow,&Paint);
-    }break;
-    default:
-    {
-      OutputDebugStringA("default");
-      Result = DefWindowProc(hWindow,uMessage,wParam,lParam);
-    }break;
-    
-  }
+  //wc.lpfnWndProc = WindowProc; &THIS HAS TO BE DEFINED NEXT ALONG WITH THE MESSAGE DECODING&
+  //https://learn.microsoft.com/en-us/windows/win32/learnwin32/window-messages
+  //https://learn.microsoft.com/en-us/windows/win32/learnwin32/writing-the-window-procedure
 
-  return Result;
-}
+  wc.hInstance = hInstance;
+  wc.lpszClassName = CLASS_NAME;
 
-//CONSIDER WHAT IS GOING ON UP THE CALLSTACK
+  RegisterClass(&wc);
 
-//an entry point windows graphical application
-//called by C runtime library(CRT)
-int CALLBACK 
-WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR     lpCmdLine,
-  int       nShowCmd)
-  {
+  HWND hwnd = CreateWindowEx(
+    0,
+    CLASS_NAME,
+    L"Window text",
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    NULL,
+    NULL,
+    hInstance,
+    NULL
+  );
 
-    //MessageBoxA(0,"Hello world!","Title",MB_OK|MB_ICONINFORMATION);
-
-    //Is a struct, = {} initializes all members to "0 value"
-    WNDCLASSA Window = {};
-
-    //Recheck later
-    Window.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-    Window.lpfnWndProc = Wndproc;
-    //Window.cbClsExtra = 0;
-    //Window.cbWndExtra = 0;
-    Window.hInstance = hInstance;
-    //Window.hIcon = 0; //Icon of an app
-    //Window.hCursor = 0; //Important for later, mouse cursor
-    //Window.hbrBackground = 0; //Background color?
-    //Window.lpszMenuName = 0;
-    Window.lpszClassName = "idk";
-
-    if (RegisterClassA(&Window)){
-   
-    HWND WindowHandle = CreateWindowExA(
-          0,
-          Window.lpszClassName,
-          "LL",
-          WS_OVERLAPPEDWINDOW|WS_VISIBLE,
-          CW_USEDEFAULT,
-          CW_USEDEFAULT,
-          CW_USEDEFAULT,
-          CW_USEDEFAULT,
-          0,
-          0,
-          hInstance,
-          0);
-          //Check if not NULL
-          if(WindowHandle){
-            MSG Message;
-            for(;;){
-              BOOL MSGRES = GetMessageA(&Message,0,0,0);
-              if (MSGRES>0){
-                TranslateMessage(&Message);
-                DispatchMessage(&Message);
-              }
-              else{
-                break;
-              }
-            }
-          }
-          else{
-            //TODO
-          }
-    }
-    else{
-      //TODO
-    }
+  if (hwnd == NULL){
     return 0;
   }
+
+  ShowWindow(hwnd, nCmdShow);
+
+  return 0;
+}
