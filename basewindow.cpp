294b577eda3df2aca0c132ead3a2f5804acff1e9
unknown <linakoskar@gmail.com>
2023-09-06 23:02:40 +0200

    Recheck

--- basewindow.cpp
+++ basewindow.cpp
@@ -1,6 +1,60 @@
 #include "basewindow.h"
 
 template <class DERIVED_TYPE>
-BaseWindow<DERIVED_TYPE>::BaseWindow(){
+LRESULT CALLBACK BaseWindow<DERIVED_TYPE>::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    DERIVED_TYPE *pThis = nullptr;
 
+    if (uMsg == WM_NCCREATE)
+    {
+        CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+        pThis = static_cast<DERIVED_TYPE*>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pThis));
+        pThis->m_hwnd = hwnd;
+    }
+    else
+    {
+        pThis = reinterpret_cast<DERIVED_TYPE*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+    }
+
+    if (pThis)
+    {
+        return pThis->HandleMessage(uMsg, wParam, lParam);
+    }
+    else
+    {
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+template <class DERIVED_TYPE>
+BaseWindow<DERIVED_TYPE>::BaseWindow() : m_hwnd(nullptr) {}
+
+template <class DERIVED_TYPE>
+BOOL BaseWindow<DERIVED_TYPE>::Create(
+    PCWSTR lpWindowName,
+    DWORD dwStyle,
+    DWORD dwExStyle,
+    int x,
+    int y,
+    int nWidth,
+    int nHeight,
+    HWND hWndParent,
+    HMENU hMenu
+)
+{
+    WNDCLASS wc = { 0 };
+
+    wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.lpszClassName = ClassName();
+
+    RegisterClass(&wc);
+
+    m_hwnd = CreateWindowEx(
+        dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+        nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(nullptr), this
+    );
+
+    return (m_hwnd ? TRUE : FALSE);
 }
--- basewindow.h
+++ basewindow.h
@@ -1,10 +1,41 @@
 #ifndef BASE_WINDOW_H
 #define BASE_WINDOW_H
 
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <tchar.h>
+#include <Windows.h>
+#include <new>
+
 template <class DERIVED_TYPE>
 class BaseWindow{
 public:
     BaseWindow();
+    //Destructor can be overridden, is it good?
+    virtual ~BaseWindow() {}
+
+    //For what?
+    BOOL Create{
+        PCWSTR lpWindowName,
+        DWORD dwStExStyle = 0,
+        int x = CW_USEDEFAULT,
+        int y = CW_USEDEFAULT,
+        int nWidth = CW_USEDEFAULT,
+        int nHeigth = CW_USEDEFAULT,
+        HWND hWndParent = 0,
+        HMENU hMenu = 0
+    };
+
+    HWND Window() const {return m_hwnd; }
+//public vs protected
+
+protected:
+    virtual PCWSTR ClassName() const = 0;
+    virtual LRESULT HndleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+
+    HWND m_hwnd;
 };
 
 #endif // BASE_WINDOW_H
