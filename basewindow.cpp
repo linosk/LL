44c6ae6f3a0210d3e8fb91b183f8f236c17d4209
unknown <linakoskar@gmail.com>
2023-09-10 16:31:37 +0200

    DefWindowProc changed to DefWindowProcW and know the whole window's title appears
    mainwindow.cpp

--- basewindow.cpp
+++ basewindow.cpp
@@ -1,5 +1,7 @@
 #include "basewindow.h"
 
+#include <iostream>
+
 template <class DERIVED_TYPE>
 
 //Apperently it is enough to define that funciont is static in header file
@@ -25,7 +27,8 @@ LRESULT CALLBACK BaseWindow<DERIVED_TYPE>::WindowProc(HWND hwnd, UINT uMsg, WPAR
         return pThis->HandleMessage(uMsg,wParam,lParam);
     }
     else{
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        //Specifing Unicode characters is neccesary
+        return DefWindowProcW(hwnd, uMsg, wParam, lParam);
     }
 
 }
@@ -35,7 +38,7 @@ BaseWindow<DERIVED_TYPE>::BaseWindow() : m_hwnd(nullptr) {}
 
 template <class DERIVED_TYPE>
 BOOL BaseWindow<DERIVED_TYPE>::Create(
-    PCWSTR lpWindowName,
+        PCWSTR lpWindowName,
         DWORD dwStyle,
         DWORD dwExStyle ,
         int x,
@@ -58,64 +61,5 @@ BOOL BaseWindow<DERIVED_TYPE>::Create(
     );
 
     //Checking if m_hwnd is not NULL, if so return TRUE, otherwise FALSE
-    return (m_hwnd ? TRUE : FALSE);
+    return (m_hwnd ? TRUE : FALSE);  
 }
-/*
-LRESULT CALLBACK BaseWindow<DERIVED_TYPE>::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    DERIVED_TYPE *pThis = nullptr;
-
-    if (uMsg == WM_NCCREATE)
-    {
-        CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
-        pThis = static_cast<DERIVED_TYPE*>(pCreate->lpCreateParams);
-        SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pThis));
-        pThis->m_hwnd = hwnd;
-    }
-    else
-    {
-        pThis = reinterpret_cast<DERIVED_TYPE*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-    }
-
-    if (pThis)
-    {
-        return pThis->HandleMessage(uMsg, wParam, lParam);
-    }
-    else
-    {
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
-}
-
-template <class DERIVED_TYPE>
-BaseWindow<DERIVED_TYPE>::BaseWindow() : m_hwnd(nullptr) {}
-
-template <class DERIVED_TYPE>
-BOOL BaseWindow<DERIVED_TYPE>::Create(
-    PCWSTR lpWindowName,
-    DWORD dwStyle,
-    DWORD dwExStyle,
-    int x,
-    int y,
-    int nWidth,
-    int nHeight,
-    HWND hWndParent,
-    HMENU hMenu
-)
-{
-    WNDCLASS wc = { 0 };
-
-    wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
-    wc.hInstance = GetModuleHandle(nullptr);
-    wc.lpszClassName = ClassName();
-
-    RegisterClass(&wc);
-
-    m_hwnd = CreateWindowEx(
-        dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
-        nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(nullptr), this
-    );
-
-    return (m_hwnd ? TRUE : FALSE);
-}
-*/
