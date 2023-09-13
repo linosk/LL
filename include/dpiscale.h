3c638eccd218d72dc122d65d863dc72c3a54996a
unknown <linakoskar@gmail.com>
2023-09-13 20:29:35 +0200

    Finish DPI_Scale

--- include/dpiscale.h
+++ include/dpiscale.h
@@ -1,8 +1,22 @@
 #ifndef DPI_SCALE_H
 #define DPI_SCALE_H
 
+//Is it good idea to declare it second time?
+//Make hader with all includes?
+#include <Windows.h>
+
+#include <d2d1.h>
+#pragma comment(lib, "d2d1")
+#include <algorithm>
+
 class DPI_Scale{
-    static float scale;
+        static float scale;
+
+    public:
+        static void Initialize(HWND hwnd);
+
+        template <template T>
+        static D2D1_POINT_2F PixelsToDips(T x, T y);
 };
 
 #endif
